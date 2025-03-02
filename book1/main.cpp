5aaf1917740231d85aab3cc34acecb8c066f5c5f
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-03-02 17:14:04 +0300

    added some logs (not visible bc current buffering and speed

--- book1/main.cpp
+++ book1/main.cpp
@@ -12,6 +12,10 @@ int main () {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) { // every pixel in height
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::endl;
+        // clog = character log, flush to update in real time
+        // doesn't display too often because it's buffered
+        // book uses std::flush but i'll use endl
         for (int i = 0; i < image_width; i++) { // every pixel in width
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
@@ -24,4 +28,6 @@ int main () {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.\t\n";
 }
