c6fe5dc7277c4535ace8f1742e435c2d98b1f4e5
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-03-07 14:38:01 +0300

    implemented random number generators

--- book1/rtweekend.h
+++ book1/rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -21,6 +22,24 @@ inline double degrees_to_radians(double degrees)
 // inline = expands in line when called instead of sent back to the object
 {
     return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    // return random real in [0,1)
+    return std::rand() / (RAND_MAX + 1.0);
+
+    // never reaches 1.0 bc rand() will always be smaller than RAND_MAX+1.0
+}
+
+inline double random_double(double min, double max)
+{
+    // return random real in [min, max)
+    return min + (max - min) * random_double();
+
+    // random_double gives us a value r between [0,1), (max-min) is the range we want
+    // (max-min) * r gives us random offset wihin our desired range
+    // adding min means we'll always be larger than our min value
 }
 
 // common headers
