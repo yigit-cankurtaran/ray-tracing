7297b6e770f0e0a6cca2059d8e177a60bba5b80e
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-03-08 15:53:47 +0300

    created an air bubble inside the glass sphere

--- book1/main.cpp
+++ book1/main.cpp
@@ -12,14 +12,19 @@ int main()
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    // dielectric object (air bubble) in water
-    auto material_left = make_shared<dielectric>(1.0 / 1.33);
+    // glass
+    auto material_left = make_shared<dielectric>(1.50);
+    // the air inside the glass
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
     // reflective metal object
     auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
+    // glass sphere
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    // same center, smaller radius for the bubble inside glass sphere
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     camera cam;
--- book1/material.h
+++ book1/material.h
@@ -89,7 +89,7 @@ public:
         bool cannot_refract = ri * sin_theta > 1.0;
         vec3 direction;
 
-        if (cannot_refract)
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double())
             direction = reflect(unit_direction, rec.normal);
         else
             direction = refract(unit_direction, rec.normal, ri);
@@ -101,6 +101,14 @@ public:
 private:
     // refractive index in vacuum or air, or the ratio of the two media the ray goes through
     double refraction_index;
+
+    // schlick's approximation for reflectance
+    static double reflectance(double cosine, double refraction_index)
+    {
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+    }
 };
 
 #endif
